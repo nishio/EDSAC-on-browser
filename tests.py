c2a674b694a1e4b00cb1666d75bbe12df7080679
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-24 20:57:19 +0900

    nice assert

--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
 from edsac import Edsac
 
+class Assert(object):
+    def __init__(self, value):
+        self.value = value
+
+    def equal(self, v):
+        assert self.value == v, "expect %r but %r" % (v, self.value)
 
 def _test_initial_order(edsac):
     edsac.load_initial_order()
@@ -8,56 +14,63 @@ def _test_initial_order(edsac):
     for i in range(3):
          # put 10<<11 in R
         edsac.step()
-    assert edsac.multiplier.high.as_number() == 10 << 11
 
-    edsac.step() # 5: goto 6
-    assert edsac.sequence_control == 6
+
+    Assert(edsac.multiplier.high.as_number()).equal(10 << 11)
+
+    edsac.step() # 5: goto A
+    Assert(edsac.sequence_control).equal(6)
 
     edsac.step()
-    edsac.step() # 7: read T(5) in m[0]
-    assert edsac.get_memory(0).bits[-5:] == [0, 0, 1, 0, 1]
+    edsac.step() # 7: read T(5) in m[A]
+    Assert(edsac.get_memory(0).bits[-5:]).equal([0, 0, 1, 0, 1])
     edsac.step() # 8: A += m[0]
-    assert edsac.get_accumulator().bits[-5:] == [0, 0, 1, 0, 1]
+    Assert(edsac.get_accumulator().bits[-5:]).equal([0, 0, 1, 0, 1])
     edsac.step() # 9: ABC >>= 6
     print edsac.get_accumulator(True)
-    assert repr(edsac.get_accumulator(True)) == "00101000000000000 0 00000000000000000"
+    Assert(repr(edsac.get_accumulator(True))).equal("00101000000000000 0 00000000000000000")
     edsac.step() # 10: w[0] = AB; ABC=0
     # (9, 10) Shift and store it, so that it becomes the senior 5 bit of m[0]
-    assert repr(edsac.get_memory(0)) == "00101 0 0000000000 0"
+    Assert(repr(edsac.get_memory(0))).equal("00101 0 0000000000 0")
     # m[1] is now 0
-    assert edsac.get_memory(1).as_number() == 0
+    Assert(edsac.get_memory(1).as_number()).equal(0)
 
     edsac.step() # 11: read 1 into m[2]
-    assert edsac.get_memory(2).bits[-5:] == [0, 0, 0, 0, 1]
+    Assert(edsac.get_memory(2).bits[-5:]).equal([0, 0, 0, 0, 1])
     edsac.step() # 12: A+=m[2]
-    assert edsac.get_accumulator().bits[-5:] == [0, 0, 0, 0, 1]
+    Assert(edsac.get_accumulator().bits[-5:]).equal([0, 0, 0, 0, 1])
     edsac.step() # 13: A-=m[5]
-    assert edsac.get_memory(5).as_bits_string() == "00000000000001010"
+    Assert(edsac.get_memory(5).as_bits_string()).equal("00000000000001010")
     edsac.step() # 14: E21S (jump to 21 if it's not a number)
-    assert edsac.sequence_control == 15 # not jump
-    assert edsac.get_memory(1).as_number() == 0 # total number
-    assert edsac.get_memory(2).as_number() == 1 # this digit
+    # not jump
+    Assert(edsac.sequence_control).equal(15)
+    # total number
+    Assert(edsac.get_memory(1).as_number()).equal(0)
+    # this digit
+    Assert(edsac.get_memory(2).as_number()).equal(1)
     edsac.step() # 15: T3S (clear A)
     edsac.step() # 16: AB+=m[1]*R1
-    assert edsac.get_accumulator().as_number() == 0
+    Assert(edsac.get_accumulator().as_number()).equal(0)
     edsac.step()
     edsac.step()
     edsac.step()
     edsac.step() # 20
-    assert edsac.get_memory(1).as_number() == 1 # total number is now 1
-    assert edsac.sequence_control == 11 # jumped to 11 and ready to read next digit
+    # total number is now 1
+    Assert(edsac.get_memory(1).as_number()).equal(1)
+    # jumped to 11 and ready to read next digit
+    Assert(edsac.sequence_control).equal(11)
 
     edsac.step() # 11: read 1 into m[2]
-    assert edsac.get_memory(2).as_number() == 2 # this digit is 2
+    # this digit is 2
+    Assert(edsac.get_memory(2).as_number()).equal(2)
     while edsac.sequence_control != 17:
         edsac.step()
 
     edsac.step()
     edsac.step()
     edsac.step()
-
-    print edsac.get_memory(1).as_number() # total number is now 12
-    assert edsac.get_memory(1).as_number() ==12 # total number is now 12
+    # total number is now 12
+    Assert(edsac.get_memory(1).as_number()).equal(2)
 
 def _test(edsac):
     import doctest

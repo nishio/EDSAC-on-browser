c78814ba0a915ec314ab48e61500c2447a2ba19a
Pawel Marczewski <humpolec@gmail.com>
2012-08-01 23:15:20 +0200

    refactoring tests

--- edsac-test.js
+++ edsac-test.js
@@ -6,47 +6,52 @@ edsac.assertEqual = function(e1, e2) {
     }
 };
 
+edsac.assertBinary = function(e1, s) {
+    edsac.assertEqual(e1.printBinary(), s);
+};
+
 edsac.test = function() {
     console.log('Running tests...');
 
     var v = edsac.zeroValue(5);
-    edsac.assertEqual(v.printBinary(), '00000');
+    var B = edsac.valueFromBinary;
+
+    edsac.assertBinary(v, '00000');
     edsac.assertEqual(v.get(4), 0);
     v.set(4, 1);
     edsac.assertEqual(v.get(4), 1);
-    edsac.assertEqual(v.printBinary(), '10000');
+    edsac.assertBinary(v, '10000');
 
-    v = edsac.valueFromBinary('101010');
-    edsac.assertEqual(v.printBinary(), '101010');
+    v = B('101010');
+    edsac.assertBinary(v, '101010');
 
-    v.add(edsac.valueFromBinary('011'));
-    edsac.assertEqual(v.printBinary(), '101101');
-    v.add(edsac.valueFromBinary('010011'));
-    edsac.assertEqual(v.printBinary(), '000000');
+    v.add(B('011'));
+    edsac.assertBinary(v, '101101');
+    v.add(B('010011'));
+    edsac.assertBinary(v, '000000');
 
-    v.add(edsac.valueFromBinary('1')); // -1: the first bit is the sign bit
-    edsac.assertEqual(v.printBinary(), '111111');
+    v.add(B('1')); // -1: the first bit is the sign bit
+    edsac.assertBinary(v, '111111');
 
-    v.sub(edsac.valueFromBinary('010'));
-    edsac.assertEqual(v.printBinary(), '111101');
+    v.sub(B('010'));
+    edsac.assertBinary(v, '111101');
 
-    v.sub(edsac.valueFromBinary('10')); // -2
-    edsac.assertEqual(v.printBinary(), '111111');
+    v.sub(B('10')); // -2
+    edsac.assertBinary(v, '111111');
 
     v.shiftLeft(2);
-    edsac.assertEqual(v.printBinary(), '111100');
+    edsac.assertBinary(v, '111100');
 
     v.shiftRight(1);
-    edsac.assertEqual(v.printBinary(), '111110');
+    edsac.assertBinary(v, '111110');
 
-    edsac.assertEqual(edsac.valueMult(edsac.valueFromBinary('011'),
-                                      edsac.valueFromBinary('0101'))
-                      .printBinary(),
+    edsac.assertBinary(edsac.valueMult(B('011'),
+                                       B('0101')),
                       '0001111');
 
-    v = edsac.valueFromBinary('00011');
-    v.mult(edsac.valueFromBinary('0101'));
-    edsac.assertEqual(v.printBinary(), '01111');
+    v = B('00011');
+    v.mult(B('0101'));
+    edsac.assertBinary(v, '01111');
 
     console.log('All tests OK!');
 };

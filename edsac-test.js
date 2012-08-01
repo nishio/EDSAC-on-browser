09a40381d47e78bac2d7acaf641c70ac88cbf63e
Pawel Marczewski <humpolec@gmail.com>
2012-08-01 21:08:27 +0200

    tests, and HTML file

---
+++ edsac-test.js
@@ -0,0 +1,20 @@
+
+edsac.assertEqual = function(e1, e2) {
+    if (e1 != e2) {
+        var err = 'Assertion failed: ' + e1 + ' != ' + e2;
+        throw err;
+    }
+};
+
+edsac.test = function() {
+    console.log('Running tests...');
+
+    var v = edsac.zeroValue(5);
+    edsac.assertEqual(v.printBinary(), '00000');
+    edsac.assertEqual(v.get(4), 0);
+    v.set(4, 1);
+    edsac.assertEqual(v.get(4), 1);
+    edsac.assertEqual(v.printBinary(), '10000');
+
+    console.log('All tests OK!');
+};

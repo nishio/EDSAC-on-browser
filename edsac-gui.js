951cb27fa05e5da0cf4e276a62cc94bc7e496137
Pawel Marczewski <humpolec@gmail.com>
2012-08-31 14:57:43 +0200

    displaying status (IP, ABC, RS)

--- edsac-gui.js
+++ edsac-gui.js
@@ -6,14 +6,17 @@ edsac.gui = {};
 edsac.gui.active = false;
 
 // Initialize the interface. The arguments are jQuery elements.
-edsac.gui.init = function(memory) {
+edsac.gui.init = function(memory, status) {
     this.memory = memory;
+    this.status = status;
 
     // For now, only show memory in 'narrow' mode
     for (var i = 0; i < 2*edsac.machine.MEM_SIZE; ++i) {
         var item = this.makeMemItem(i);
         memory.append(item);
     }
+
+    this.updateStatus();
 
     this.active = true;
 };
@@ -46,4 +49,35 @@ edsac.gui.makeMemItem = function(addr) {
         elt.addClass('current');
 
     return elt;
+};
+
+edsac.gui.updateStatus = function() {
+    var html = '';
+    html += 'IP = ' + edsac.formatInt(edsac.machine.ip, 4) + '<br>';
+    html += '<br>';
+
+    var abc = edsac.machine.getAccum(2).printBinary();
+    html += 'ABC: [' + (abc.substr(0,17) + ' ' +
+                         abc.substr(17,1) + ' ' +
+                        abc.substr(18,17) + ' ' +
+                         abc.substr(35,1) + ' ' +
+                        abc.substr(36,17) + ' ' +
+                         abc.substr(53,1) + ' ' +
+                        abc.substr(54,17)) + ']<br>';
+
+    html += ('ABC = ' + edsac.machine.getAccum(2).printDecimal(true) +
+             ', AB = ' + edsac.machine.getAccum(1).printDecimal(true) +
+             ', A = ' + edsac.machine.getAccum(1).printDecimal(true)) + '<br>';
+    html += '<br>';
+
+    var rs = edsac.machine.getMult(1).printBinary();
+    html += 'RS: &nbsp;[' + (rs.substr(0,17) + ' ' +
+                              rs.substr(17,1) + ' ' +
+                             rs.substr(18,17)) + ']<br>';
+
+    html += ('RS = ' + edsac.machine.getMult(1).printDecimal(true) +
+             ', R = ' + edsac.machine.getMult(0).printDecimal(true)) + '<br>';
+    html += '<br>';
+
+    this.status.html(html);
 };

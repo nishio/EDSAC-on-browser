4f34069a3f9d7dfd484a5fa125c1cd5cb1fba43c
Pawel Marczewski <humpolec@gmail.com>
2012-08-31 14:46:03 +0200

    highlighting current IP

--- edsac-gui.js
+++ edsac-gui.js
@@ -3,14 +3,19 @@
 
 edsac.gui = {};
 
+edsac.gui.active = false;
+
 // Initialize the interface. The arguments are jQuery elements.
 edsac.gui.init = function(memory) {
+    this.memory = memory;
+
     // For now, only show memory in 'narrow' mode
     for (var i = 0; i < 2*edsac.machine.MEM_SIZE; ++i) {
         var item = this.makeMemItem(i);
         memory.append(item);
     }
-    memory.find('.mem-item:odd').addClass('odd');
+
+    this.active = true;
 };
 
 edsac.formatInt = function(n, width) {
@@ -27,10 +32,18 @@ edsac.gui.MEM_TEMPLATE =
 
 edsac.gui.makeMemItem = function(addr) {
     var val = edsac.machine.get(addr, false);
-    var code = (this.MEM_TEMPLATE.replace('%addr%', String(addr))
+    var code = (this.MEM_TEMPLATE
+                .replace('%addr%', String(addr))
                 .replace('%addr0%', edsac.formatInt(addr,4))
                 .replace('%bytes%', val.printOrderBinary())
                 .replace('%order%', val.printOrder()));
 
-    return $(code);
+    var elt = $(code);
+    if (addr % 2 == 1)
+        elt.addClass('odd');
+
+    if (addr == edsac.machine.ip)
+        elt.addClass('current');
+
+    return elt;
 };
